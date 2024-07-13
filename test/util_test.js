7f793d627989eca761680e16387657703bdf9cdc
Leslie Chou <lesliechou@LesliedeMacBook-Pro.local>
2024-07-13 13:20:36 +0800

    try to fail test

--- test/util_test.js
+++ test/util_test.js
@@ -4,6 +4,6 @@ var calculator = require('../utils/calculator');
 describe('Add Calculator', function () {
   it('Add two positive numbers', function () {
     let sum = calculator.add(1, 4);
-    expect(sum).to.equal(8);
+    expect(sum).to.equal(9);
   });
 });
