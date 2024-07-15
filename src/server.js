e582768804a016077f2b87fc7a5f00e97a8a3f13
Leslie Chou <lesliechou@LesliedeMacBook-Pro.local>
2024-07-15 14:15:22 +0800

    add start server.js command

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello Pipeline, test is passed');
+  res.send('Hello Pipeline, test is passed, good');
 });
 
 app.get('/db', (req, res) => {
